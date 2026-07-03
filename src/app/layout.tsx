c97a2f58488e11a3fd49b8527e714f6d28a966ba
Duong Phu Dong <dongduong840@gmail.com>
2026-07-03 17:21:09 +0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Be_Vietnam_Pro, Noto_Serif_JP } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
+import { Analytics } from "@vercel/analytics/react";
 
 import { BRAND_NAME, SITE_URL } from "@/lib/constants";
 import "./globals.css";
@@ -78,6 +79,7 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <Analytics />
       </body>
     </html>
   );
